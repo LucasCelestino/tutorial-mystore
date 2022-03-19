752e5fe581bfdc65fef7b6b39d36b5c4185ddf3e
LucasCelestino <lucasrashido@gmail.com>
2022-03-19 12:41:19 -0300

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ProductListComponent,
+    TopBarComponent,
+  ],
+  bootstrap: [ProductListComponent],
 })
-export class AppModule { }
+export class AppModule {}
