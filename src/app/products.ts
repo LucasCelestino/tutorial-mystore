42df96677e7986439ae05352a923c9d94029888f
LucasCelestino <lucasrashido@gmail.com>
2022-03-19 12:47:52 -0300

    1 file added

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: 'Lucas Celestino Phone',
+    description: 'A Lucas Celestino Phone',
+  },
+  {
+    id: 2,
+    name: 'Phone XL',
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 3,
+    name: 'Phone Mini',
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 4,
+    name: 'Phone Standard',
+    description: '',
+  },
+];
